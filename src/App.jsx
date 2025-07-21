cd8c9da3ee14e1aea107799cff9d9e8b60132873
justbappy <yennamvishal1883@gmail.com>
2025-07-22 00:23:13 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { Route, Routes, useParams } from 'react-router-dom'
+import { HashRouter, Route, Routes, useParams } from 'react-router-dom'
 import './App.css'
 import Landing from './pages/Landing/Landing'
-import { useState } from 'react'
 import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
@@ -9,13 +8,15 @@ import Login from './pages/Login/Login';
 function App() {
   return (
   <>
-    <Routes>
-      <Route path={'/'} exact={true} element={
-        localStorage.getItem('username')?<Home/>:<Landing/>
-      }/>
-      <Route path='/register' exact={true} element={<Register/>}/>
-      <Route path='/login' exact={true} element={<Login/>}/>
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path={'/'} exact={true} element={
+          localStorage.getItem('username')?<Home/>:<Landing/>
+        }/>
+        <Route path='/register' exact={true} element={<Register/>}/>
+        <Route path='/login' exact={true} element={<Login/>}/>
+      </Routes>
+    </HashRouter>
   </>
 )
 }
